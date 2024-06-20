43219556a4c18fada21cf3dddf4729e1211352cf
Trần Minh Sơn <sontm@tahospital.vn>
2024-06-20 15:00:41 +0700

    remove old project

--- SONT.WEB/Entities/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace SONT.WEB.Entities
-{
-    public class ApplicationUser : IdentityUser
-    {
-        // Add additional user properties here
-    }
-}
--- SONT.WEB/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SONT.WEB.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
